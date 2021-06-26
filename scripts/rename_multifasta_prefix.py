2575ff1141ad9bf649a04a390200646b54cc85c9
Chengkaizhu <chengkaizhu0624@gmail.com>
2021-06-27 00:26:04 +0800

    Update rename_multifasta_prefix.py

--- scripts/rename_multifasta_prefix.py
+++ scripts/rename_multifasta_prefix.py
@@ -1,20 +1,4 @@
-#!/usr/bin/env python2
-
-# This file is part of MAG Snakemake workflow.
-#
-# MAG Snakemake workflow is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# MAG Snakemake workflow is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with MAG Snakemake workflow.  If not, see <https://www.gnu.org/licenses/>.
-
+#!/usr/bin/env python3
 
 import argparse
 import sys
@@ -25,9 +9,9 @@ def ren_fasta(args):
 		if line[0] == ">":
 			name = line.strip("\n").replace(">","")
 			n += 1
-			print ">%s_%i\t%s" % (args.prefix, n, name)
+			print (">%s_%i\t%s" % (args.prefix, n, name))
 		else:
-			print line.strip("\n")
+			print (line.strip("\n"))
 
 
 if __name__ == '__main__':
